b3f63bd3a39c30aefb7636cf6084b20668febde6
Julia-Tryan <uliana.troyan@mail.ru>
2023-10-23 13:18:45 +0300

    решение первой задачи

--- Lesson c# 3/Program.cs
+++ Lesson c# 3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая принимает 
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// тип возвращающего значения + Название + () + {}
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetSum()
+{
+    int start = 1;
+    int sum=0;
+     
+    while(start<= num)
+    {
+        sum += start;
+     // sum = sum +start;
+        start++;
+    }
+    return sum;
+}
+
+// int result= GetSum();
+// System.Console.WriteLine(result);
+
+System.Console.WriteLine(GetSum());
